a9764906be9b26a05e17c9a56e45218e6532a976
Tyler Matteo <tylermatteo434@gmail.com>
2023-10-27 14:33:19 -0400

    Set up Config and MikroOrm modules. Add dependencies and template env file. Update README.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import * as Joi from "joi";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("development", "production", "test"),
+        DATABASE_HOST: Joi.string(),
+        DATABASE_NAME: Joi.string(),
+        DATABASE_PASSWORD: Joi.string(),
+        DATABASE_PORT: Joi.number(),
+        DATABASE_USER: Joi.string(),
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgresql",
+        dbName: configService.get("DATABASE_NAME"),
+        user: configService.get("DATABASE_USER"),
+        password: configService.get("DATABASE_PASSWORD"),
+        host: configService.get("DATABASE_HOST"),
+        port: configService.get("DATABASE_PORT"),
+        entities: ["dist/**/*.entity.js"],
+        entitiesTs: ["src/**/*.entity.ts"],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
