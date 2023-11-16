ff13238da0af765d051d88c249636ec65c84fcbb
tangoyankee <miller.tim108@gmail.com>
2023-11-16 13:43:07 -0500

    configure drizzle migrations and create borough schema

---
+++ drizzle-management/pg-client.ts
@@ -0,0 +1,10 @@
+import { Client } from "pg";
+import "dotenv/config";
+
+export const pgClient = new Client({
+  host: process.env.DATABASE_HOST!,
+  port: parseInt(process.env.DATABASE_PORT!),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME!,
+});
---
+++ src/schema/borough.ts
@@ -0,0 +1,10 @@
+import { InferSelectModel } from "drizzle-orm";
+import { char, pgTable, text } from "drizzle-orm/pg-core";
+
+export const borough = pgTable("borough", {
+  id: char("id", { length: 1 }).primaryKey(),
+  title: text("title").notNull(),
+  abbr: text("abbr").notNull(),
+});
+
+export type SelectBorough = InferSelectModel<typeof borough>;
