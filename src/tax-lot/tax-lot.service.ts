9ae2f1832cf03ec5a2eb1dfa0f09f1ae7685562e
tangoyankee <miller.tim108@gmail.com>
2023-11-17 10:08:36 -0500

    Support boroughs route in drizzle Throw error when accessing endpoints that are not supported by drizzle

--- src/tax-lot/tax-lot.service.ts
+++ src/tax-lot/tax-lot.service.ts
@@ -1,16 +1,23 @@
 import { InjectRepository } from "@mikro-orm/nestjs";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { TaxLot } from "./tax-lot.entity";
 import { TaxLotRepository } from "./tax-lot.repository";
+import { FeatureFlagConfig } from "src/config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export class TaxLotService {
   constructor(
     @InjectRepository(TaxLot)
     private readonly taxLotRepository: TaxLotRepository,
+
+    @Inject(FeatureFlagConfig.KEY)
+    private featureFlagConfig: ConfigType<typeof FeatureFlagConfig>,
   ) {}
 
   async findTaxLotByBbl(bbl: string): Promise<TaxLot | null> {
+    if (this.featureFlagConfig.useDrizzle)
+      throw new Error("Tax lot route not support in drizzle");
     return this.taxLotRepository.findOne(
       { bbl },
       {
@@ -21,6 +28,8 @@ export class TaxLotService {
   }
 
   async findTaxLotByBblGeoJson(bbl: string): Promise<TaxLot | null> {
+    if (this.featureFlagConfig.useDrizzle)
+      throw new Error("Tax lot geojson route not support in drizzle");
     return this.taxLotRepository.findOne({ bbl });
   }
 }
