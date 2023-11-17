586858472caa3a336b4f57ce3d6cacac63ecd9ec
tangoyankee <miller.tim108@gmail.com>
2023-11-17 10:08:36 -0500

    Support boroughs route in drizzle Throw error when accessing endpoints that are not supported by drizzle

--- src/borough/borough.service.ts
+++ src/borough/borough.service.ts
@@ -1,16 +1,29 @@
 import { InjectRepository } from "@mikro-orm/nestjs";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { Borough } from "./borough.entity";
 import { BoroughRepository } from "./borough.repository";
+import { DB, DbType } from "src/global/providers/db.provider";
+import { FeatureFlagConfig } from "src/config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export class BoroughService {
   constructor(
     @InjectRepository(Borough)
     private readonly boroughRepository: BoroughRepository,
+
+    @Inject(DB)
+    private readonly db: DbType,
+
+    @Inject(FeatureFlagConfig.KEY)
+    private featureFlagConfig: ConfigType<typeof FeatureFlagConfig>,
   ) {}
 
-  async findAll(): Promise<Borough[]> {
-    return this.boroughRepository.findAll();
+  async findAll() {
+    if (this.featureFlagConfig.useDrizzle) {
+      return this.db.query.borough.findMany();
+    } else {
+      return this.boroughRepository.findAll();
+    }
   }
 }
--- src/land-use/land-use.service.ts
+++ src/land-use/land-use.service.ts
@@ -1,16 +1,23 @@
 import { InjectRepository } from "@mikro-orm/nestjs";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { LandUse } from "./land-use.entity";
 import { LandUseRepository } from "./land-use.repository";
+import { FeatureFlagConfig } from "src/config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export class LandUseService {
   constructor(
     @InjectRepository(LandUse)
     private readonly landUseRepository: LandUseRepository,
+
+    @Inject(FeatureFlagConfig.KEY)
+    private featureFlagConfig: ConfigType<typeof FeatureFlagConfig>,
   ) {}
 
   async findAll(): Promise<LandUse[]> {
+    if (this.featureFlagConfig.useDrizzle)
+      throw new Error("Land uses route not support in drizzle");
     return this.landUseRepository.findAll();
   }
 }
