1d9029be4728ae0eb3a30b3fdd4680cb86d8b6cc
tangoyankee <miller.tim108@gmail.com>
2023-11-20 09:34:32 -0500

    Create tax lot endpoint in drizzle

--- src/tax-lot/tax-lot.service.ts
+++ src/tax-lot/tax-lot.service.ts
@@ -1,6 +1,7 @@
 import { InjectRepository } from "@mikro-orm/nestjs";
 import { Inject, Injectable } from "@nestjs/common";
 import { TaxLot } from "./tax-lot.entity";
+import { DB, DbType } from "src/global/providers/db.provider";
 import { TaxLotRepository } from "./tax-lot.repository";
 import { FeatureFlagConfig } from "src/config";
 import { ConfigType } from "@nestjs/config";
@@ -11,20 +12,38 @@ export class TaxLotService {
     @InjectRepository(TaxLot)
     private readonly taxLotRepository: TaxLotRepository,
 
+    @Inject(DB)
+    private readonly db: DbType,
+
     @Inject(FeatureFlagConfig.KEY)
     private featureFlagConfig: ConfigType<typeof FeatureFlagConfig>,
   ) {}
 
-  async findTaxLotByBbl(bbl: string): Promise<TaxLot | null> {
-    if (this.featureFlagConfig.useDrizzle)
-      throw new Error("Tax lot route not support in drizzle");
-    return this.taxLotRepository.findOne(
-      { bbl },
-      {
-        fields: ["bbl", "borough", "landUse", "block", "lot", "address"],
-        populate: ["borough", "landUse"],
-      },
-    );
+  async findTaxLotByBbl(bbl: string) {
+    if (this.featureFlagConfig.useDrizzle) {
+      const results = await this.db.query.taxLot.findMany({
+        columns: {
+          boroughId: false,
+          landUseId: false,
+          wgs84: false,
+          liFt: false,
+        },
+        where: (taxLot, { eq }) => eq(taxLot.bbl, bbl),
+        with: {
+          borough: true,
+          landUse: true,
+        },
+      });
+      return results.length > 0 ? results[0] : null;
+    } else {
+      return this.taxLotRepository.findOne(
+        { bbl },
+        {
+          fields: ["bbl", "borough", "landUse", "block", "lot", "address"],
+          populate: ["borough", "landUse"],
+        },
+      );
+    }
   }
 
   async findTaxLotByBblGeoJson(bbl: string): Promise<TaxLot | null> {
