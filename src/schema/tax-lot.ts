efa78d123d5b1489afb8e4beddacbd83c03ef964
tangoyankee <miller.tim108@gmail.com>
2023-11-20 09:06:10 -0500

    Create tax lot schema and migration Load tax lot data

---
+++ src/schema/tax-lot.ts
@@ -0,0 +1,16 @@
+import { char, pgTable, text } from "drizzle-orm/pg-core";
+import { borough, landUse } from "../schema";
+import { multiPolygonGeog, multiPolygonGeom } from "../drizzle-pgis";
+
+export const taxLot = pgTable("tax_lot", {
+  bbl: char("bbl", { length: 10 }).primaryKey(),
+  boroughId: char("borough_id", { length: 1 })
+    .notNull()
+    .references(() => borough.id),
+  block: text("block").notNull(),
+  lot: text("lot").notNull(),
+  address: text("address"),
+  landUseId: char("land_use_id", { length: 2 }).references(() => landUse.id),
+  wgs84: multiPolygonGeog("wgs84", 4326).notNull(),
+  liFt: multiPolygonGeom("li_ft", 2263).notNull(),
+});
