8e2c7d509ceb822fba8bdc02171dead56e33cd4d
horatio <horatiorosa@aol.com>
2023-11-21 10:53:02 -0500

    remove micro orm imports and methods from app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,7 @@
-import { MikroORM } from "@mikro-orm/core";
-import { EntityManager } from "@mikro-orm/postgresql";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly orm: MikroORM,
-    private readonly em: EntityManager,
-  ) {}
-
   getHello(): string {
     return "Hello World!";
   }
