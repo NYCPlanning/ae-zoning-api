536c203d46cd3f9e18b26ca7a9cbdd8ddfd9ab67
David Hochbaum <dhochbaum@planning.nyc.gov>
2023-11-22 09:39:12 -0500

    Added zoning districts

--- src/zoning-district/zoning-district.controller.ts
+++ src/zoning-district/zoning-district.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Inject,
+  Injectable,
+  Param,
+  Redirect,
+} from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
 import { ZoningDistrictService } from "./zoning-district.service";
+import { StorageConfig } from "src/config";
 
+@Injectable()
 @Controller("zoning-districts")
 export class ZoningDistrictController {
-  constructor(private readonly zoningDistrictService: ZoningDistrictService) {}
+  constructor(
+    private readonly zoningDistrictService: ZoningDistrictService,
+    @Inject(StorageConfig.KEY)
+    private storageConfig: ConfigType<typeof StorageConfig>,
+  ) {}
 
   @Get("/:id")
   async findById(@Param() params: { id: string }) {
     return this.zoningDistrictService.findById(params.id);
+  }
+
+  @Get("/:z/:x/:y.pbf")
+  @Redirect()
+  findZoningDistrictTilesets(
+    @Param() params: { z: number; x: number; y: number },
+  ) {
+    return {
+      url: `${this.storageConfig.storageUrl}/tilesets/zoning_district/${params.z}/${params.x}/${params.y}.pbf`,
+    };
   }
 }
