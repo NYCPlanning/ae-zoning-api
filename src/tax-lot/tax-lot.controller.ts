3fb9c7342b24b8bda615b3b1283f1044932e569d
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-11-27 10:09:10 -0500

    35 Implement endpoints to redirect to tilesets (#56)
    * Added route, pending discussion
    
    * Completed tax lots
    
    * Added zoning districts

--- src/tax-lot/tax-lot.controller.ts
+++ src/tax-lot/tax-lot.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Inject,
+  Injectable,
+  Param,
+  Redirect,
+} from "@nestjs/common";
+import { ConfigType } from "@nestjs/config";
 import { TaxLotService } from "./tax-lot.service";
+import { StorageConfig } from "src/config";
 
+@Injectable()
 @Controller("tax-lots")
 export class TaxLotController {
-  constructor(private readonly taxLotService: TaxLotService) {}
+  constructor(
+    private readonly taxLotService: TaxLotService,
+    @Inject(StorageConfig.KEY)
+    private storageConfig: ConfigType<typeof StorageConfig>,
+  ) {}
 
   @Get("/:bbl")
   async findDetailsByBbl(@Param() params: { bbl: string }) {
@@ -13,5 +27,13 @@ export class TaxLotController {
   @Get("/:bbl/geojson")
   async findTaxLotByBblGeoJson(@Param() params: { bbl: string }) {
     return this.taxLotService.findTaxLotByBbl(params.bbl);
+  }
+
+  @Get("/:z/:x/:y.pbf")
+  @Redirect()
+  findTaxLotTilesets(@Param() params: { z: number; x: number; y: number }) {
+    return {
+      url: `${this.storageConfig.storageUrl}/tilesets/tax_lot/${params.z}/${params.x}/${params.y}.pbf`,
+    };
   }
 }
