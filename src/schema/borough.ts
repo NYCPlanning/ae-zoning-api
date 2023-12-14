0b711975cca9977660e3492010ab2293f88c5065
tangoyankee <miller.tim108@gmail.com>
2023-12-14 15:38:35 -0500

    Database types
    Add zod schemas and types for:
    - borough
    - land use
    - tax lots

--- src/schema/borough.ts
+++ src/schema/borough.ts
@@ -1,7 +1,13 @@
 import { char, pgTable, text } from "drizzle-orm/pg-core";
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const borough = pgTable("borough", {
   id: char("id", { length: 1 }).primaryKey(),
   title: text("title").notNull(),
   abbr: text("abbr").notNull(),
 });
+
+export const selectBoroughSchema = createSelectSchema(borough);
+
+export type SelectBorough = z.infer<typeof selectBoroughSchema>;
--- src/schema/land-use.ts
+++ src/schema/land-use.ts
@@ -1,7 +1,13 @@
 import { char, pgTable, text } from "drizzle-orm/pg-core";
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const landUse = pgTable("land_use", {
   id: char("id", { length: 2 }).primaryKey(),
   description: text("description").notNull(),
   color: char("color", { length: 9 }).notNull(),
 });
+
+export const selectLandUseSchema = createSelectSchema(landUse);
+
+export type SelectLandUse = z.infer<typeof selectLandUseSchema>;
