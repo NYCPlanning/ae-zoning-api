631decfad6c15d782b333e57f2bbd0fd3f618221
tangoyankee <miller.tim108@gmail.com>
2023-12-15 08:46:10 -0500

    Database types
    Add zod schema and type for zoning districts

--- src/schema/zoning-district.ts
+++ src/schema/zoning-district.ts
@@ -1,5 +1,7 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { multiPolygonGeog, multiPolygonGeom } from "../drizzle-pgis";
+import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const zoningDistrict = pgTable("zoning_district", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -7,3 +9,7 @@ export const zoningDistrict = pgTable("zoning_district", {
   wgs84: multiPolygonGeog("wgs84", 4326).notNull(),
   liFt: multiPolygonGeom("li_ft", 2263).notNull(),
 });
+
+export const selectZoningDistrictSchema = createSelectSchema(zoningDistrict);
+
+export type SelectZoningDistrict = z.infer<typeof selectZoningDistrictSchema>;
