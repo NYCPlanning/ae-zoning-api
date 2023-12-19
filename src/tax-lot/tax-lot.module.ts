caa0fc9ea80e42b1fe01058b3cd3d2daf83c14f3
horatio <horatiorosa@aol.com>
2023-12-19 15:25:18 -0500

    106/ Refactor Tax Lot Endpoint - moved db access from service to repo

--- src/tax-lot/tax-lot.module.ts
+++ src/tax-lot/tax-lot.module.ts
@@ -3,11 +3,12 @@ import { Module } from "@nestjs/common";
 import { TaxLot } from "./tax-lot.entity";
 import { TaxLotService } from "./tax-lot.service";
 import { TaxLotController } from "./tax-lot.controller";
+import { TaxLotRepo } from "./tax-lot.repo";
 
 @Module({
   imports: [MikroOrmModule.forFeature({ entities: [TaxLot] })],
   exports: [TaxLotService],
-  providers: [TaxLotService],
+  providers: [TaxLotService, TaxLotRepo],
   controllers: [TaxLotController],
 })
 export class TaxLotModule {}
