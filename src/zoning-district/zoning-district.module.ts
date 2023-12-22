14b4f646eb71cd507184a6276ee448650783a45e
horatio <horatiorosa@aol.com>
2023-12-22 14:52:30 -0500

    107/refactored zoning district endpoint - moving db access from the service to drizzle specific repo

--- src/zoning-district/zoning-district.module.ts
+++ src/zoning-district/zoning-district.module.ts
@@ -3,11 +3,12 @@ import { Module } from "@nestjs/common";
 import { ZoningDistrict } from "./zoning-district.entity";
 import { ZoningDistrictService } from "./zoning-district.service";
 import { ZoningDistrictController } from "./zoning-district.controller";
+import { ZoningDistrictRepo } from "./zoning-district.repo";
 
 @Module({
   imports: [MikroOrmModule.forFeature({ entities: [ZoningDistrict] })],
   exports: [ZoningDistrictService],
-  providers: [ZoningDistrictService],
+  providers: [ZoningDistrictService, ZoningDistrictRepo],
   controllers: [ZoningDistrictController],
 })
 export class ZoningDistrictModule {}
---
+++ src/zoning-district/zoning-district.repo.ts
@@ -0,0 +1,78 @@
+import { Inject } from "@nestjs/common";
+import { DB, DbType } from "src/global/providers/db.provider";
+import { eq } from "drizzle-orm";
+import { DataRetrievalException } from "src/exception";
+import {
+  zoningDistrict,
+  zoningDistrictClass,
+  zoningDistrictZoningDistrictClass,
+} from "src/schema";
+
+export class ZoningDistrictRepo {
+  constructor(
+    @Inject(DB)
+    private readonly db: DbType,
+  ) {}
+
+  #checkZoningDistrictById = this.db.query.zoningDistrict
+    .findFirst({
+      columns: {
+        id: true,
+      },
+      where: (zoningDistrict, { eq, sql }) =>
+        eq(zoningDistrict.id, sql.placeholder("id")),
+    })
+    .prepare("checkZoningDistrictById");
+
+  async checkZoningDistrictById(id: string) {
+    try {
+      return await this.#checkZoningDistrictById.execute({
+        id,
+      });
+    } catch {
+      throw new DataRetrievalException();
+    }
+  }
+
+  async findByUuid(uuid: string) {
+    try {
+      return await this.db.query.zoningDistrict.findFirst({
+        columns: { wgs84: false, liFt: false },
+        where: eq(zoningDistrict.id, uuid),
+      });
+    } catch {
+      throw new DataRetrievalException();
+    }
+  }
+
+  async findClassesByUuid(id: string) {
+    try {
+      return await this.db
+        .select({
+          id: zoningDistrictClass.id,
+          category: zoningDistrictClass.category,
+          description: zoningDistrictClass.description,
+          url: zoningDistrictClass.url,
+          color: zoningDistrictClass.color,
+        })
+        .from(zoningDistrictClass)
+        .leftJoin(
+          zoningDistrictZoningDistrictClass,
+          eq(
+            zoningDistrictZoningDistrictClass.zoningDistrictClassId,
+            zoningDistrictClass.id,
+          ),
+        )
+        .leftJoin(
+          zoningDistrict,
+          eq(
+            zoningDistrictZoningDistrictClass.zoningDistrictId,
+            zoningDistrict.id,
+          ),
+        )
+        .where(eq(zoningDistrict.id, id));
+    } catch {
+      throw new DataRetrievalException();
+    }
+  }
+}
