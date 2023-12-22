a9195646d15e2dc95647981b9938c755e6baa892
horatio <horatiorosa@aol.com>
2023-12-22 14:52:30 -0500

    107/refactored zoning district endpoint - moving db access from the service to drizzle specific repo

--- src/zoning-district/zoning-district.service.ts
+++ src/zoning-district/zoning-district.service.ts
@@ -4,27 +4,17 @@ import { ZoningDistrict } from "./zoning-district.entity";
 import { ZoningDistrictRepository } from "./zoning-district.repository";
 import { FeatureFlagConfig } from "src/config";
 import { ConfigType } from "@nestjs/config";
-import { DB, DbType } from "src/global/providers/db.provider";
-import {
-  zoningDistrict,
-  zoningDistrictClass,
-  zoningDistrictZoningDistrictClass,
-} from "src/schema";
-import { eq } from "drizzle-orm";
-import { SelectZoningDistrict } from "src/schema/zoning-district";
-import {
-  DataRetrievalException,
-  ResourceNotFoundException,
-} from "src/exception";
+import { ResourceNotFoundException } from "src/exception";
+import { ZoningDistrictRepo } from "./zoning-district.repo";
 
 @Injectable()
 export class ZoningDistrictService {
   constructor(
-    @Inject(DB)
-    private readonly db: DbType,
-
     @InjectRepository(ZoningDistrict)
     private readonly zoningDistrictRepository: ZoningDistrictRepository,
+
+    @Inject(ZoningDistrictRepo)
+    private readonly zoningDistrictRepo: ZoningDistrictRepo,
 
     @Inject(FeatureFlagConfig.KEY)
     private featureFlagConfig: ConfigType<typeof FeatureFlagConfig>,
@@ -32,17 +22,10 @@ export class ZoningDistrictService {
 
   async findZoningDistrictByUuid(uuid: string) {
     if (this.featureFlagConfig.useDrizzle) {
-      let result: SelectZoningDistrict | undefined;
-      try {
-        result = await this.db.query.zoningDistrict.findFirst({
-          columns: { wgs84: false, liFt: false },
-          where: eq(zoningDistrict.id, uuid),
-        });
-      } catch {
-        throw new DataRetrievalException();
-      }
-      if (result === undefined) throw new ResourceNotFoundException();
-      return result;
+      const zoningDistrict = await this.zoningDistrictRepo.findByUuid(uuid);
+      if (zoningDistrict === undefined) throw new ResourceNotFoundException();
+
+      return zoningDistrict;
     } else {
       return this.zoningDistrictRepository.findOne(uuid, {
         fields: ["id", "label"],
@@ -50,61 +33,18 @@ export class ZoningDistrictService {
     }
   }
 
-  #checkZoningDistrictById = this.db.query.zoningDistrict
-    .findFirst({
-      columns: {
-        id: true,
-      },
-      where: (zoningDistrict, { eq, sql }) =>
-        eq(zoningDistrict.id, sql.placeholder("id")),
-    })
-    .prepare("checkZoningDistrictById");
-
   async findClassesByZoningDistrictUuid(id: string) {
     if (this.featureFlagConfig.useDrizzle) {
-      let zoningDistrictCheck: Pick<SelectZoningDistrict, "id"> | undefined;
-      try {
-        zoningDistrictCheck = await this.#checkZoningDistrictById.execute({
-          id,
-        });
-      } catch {
-        throw new DataRetrievalException();
-      }
+      const zoningDistrictCheck =
+        await this.zoningDistrictRepo.checkZoningDistrictById(id);
       if (zoningDistrictCheck === undefined)
         throw new ResourceNotFoundException();
+      const zoningDistrictClasses =
+        await this.zoningDistrictRepo.findClassesByUuid(id);
 
-      try {
-        const zoningDistrictClasses = await this.db
-          .select({
-            id: zoningDistrictClass.id,
-            category: zoningDistrictClass.category,
-            description: zoningDistrictClass.description,
-            url: zoningDistrictClass.url,
-            color: zoningDistrictClass.color,
-          })
-          .from(zoningDistrictClass)
-          .leftJoin(
-            zoningDistrictZoningDistrictClass,
-            eq(
-              zoningDistrictZoningDistrictClass.zoningDistrictClassId,
-              zoningDistrictClass.id,
-            ),
-          )
-          .leftJoin(
-            zoningDistrict,
-            eq(
-              zoningDistrictZoningDistrictClass.zoningDistrictId,
-              zoningDistrict.id,
-            ),
-          )
-          .where(eq(zoningDistrict.id, id));
-
-        return {
-          zoningDistrictClasses,
-        };
-      } catch {
-        throw new DataRetrievalException();
-      }
+      return {
+        zoningDistrictClasses,
+      };
     } else {
       throw new Error(
         "Zoning District Classes route not supported in Mikro ORM",
