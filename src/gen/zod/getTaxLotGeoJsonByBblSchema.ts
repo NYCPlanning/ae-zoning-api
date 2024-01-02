28d21afbe850f6ed64fc3a3d1657a7ef1ba79d77
tangoyankee <miller.tim108@gmail.com>
2024-01-02 15:44:44 -0500

    129 bbl path parameter
    Create reusable bbl path parameter
    Strictly define bbl structure with regex

--- src/gen/zod/getTaxLotGeoJsonByBblSchema.ts
+++ src/gen/zod/getTaxLotGeoJsonByBblSchema.ts
@@ -4,7 +4,12 @@ import { errorSchema } from "./errorSchema";
 import { taxLotGeoJsonSchema } from "./taxLotGeoJsonSchema";
 
 export const getTaxLotGeoJsonByBblPathParamsSchema = z.object({
-  bbl: z.string().min(10).max(10),
+  bbl: z
+    .string()
+    .describe(
+      `The ten character code compromised of a one character borough, five character block, and four character lot codes.`,
+    )
+    .regex(new RegExp("^([0-9]{10})$")),
 });
 export const getTaxLotGeoJsonByBbl400Schema = z.lazy(() => errorSchema).schema;
 export const getTaxLotGeoJsonByBbl404Schema = z.lazy(() => errorSchema).schema;
--- src/gen/zod/getZoningDistrictsByTaxLotBblSchema.ts
+++ src/gen/zod/getZoningDistrictsByTaxLotBblSchema.ts
@@ -4,7 +4,12 @@ import { errorSchema } from "./errorSchema";
 import { zoningDistrictSchema } from "./zoningDistrictSchema";
 
 export const getZoningDistrictsByTaxLotBblPathParamsSchema = z.object({
-  bbl: z.string().min(10).max(10),
+  bbl: z
+    .string()
+    .describe(
+      `The ten character code compromised of a one character borough, five character block, and four character lot codes.`,
+    )
+    .regex(new RegExp("^([0-9]{10})$")),
 });
 export const getZoningDistrictsByTaxLotBbl400Schema = z.lazy(
   () => errorSchema,
