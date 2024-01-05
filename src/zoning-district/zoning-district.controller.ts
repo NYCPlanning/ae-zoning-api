e914d5a20aabb0be66a8bf7d8eb79272399be6d8
tangoyankee <miller.tim108@gmail.com>
2024-01-05 10:15:09 -0500

    protobuf header

--- src/zoning-district/zoning-district.controller.ts
+++ src/zoning-district/zoning-district.controller.ts
@@ -13,6 +13,7 @@ import { ZoningDistrictService } from "./zoning-district.service";
 import { StorageConfig } from "src/config";
 import { HttpService } from "@nestjs/axios";
 import { createReadStream } from "fs";
+import { Response } from "express";
 
 @Injectable()
 @Controller("zoning-districts")
@@ -37,16 +38,17 @@ export class ZoningDistrictController {
   }
 
   @Get("/:z/:x/:y.pbf")
-  @Header("Content-Type", "binary/octet-stream")
+  @Header("Content-Type", "application/x-protobuf")
   async findZoningDistrictTilesets(
     @Param() params: { z: number; x: number; y: number },
-    // @Res({ passthrough: true }) res: Response,
+    @Res({ passthrough: true }) res: Response,
   ) {
     console.info("get result");
     const result = await this.httpService.axiosRef.get(
       `${this.storageConfig.storageUrl}/tilesets/zoning_district/${params.z}/${params.x}/${params.y}.pbf`,
     );
-    console.info("the result is:", result);
-    return result.data;
+    // console.info("the result is:", result);
+    res.send(result.data);
+    // return result.data;
   }
 }
