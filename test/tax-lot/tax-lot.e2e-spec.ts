88aff3b4a18130f313aeb881e09e689e7181cb9b
tangoyankee <miller.tim108@gmail.com>
2024-01-22 11:47:31 -0500

    146
    Testing: unit and e2e for getTaxLotByBbl endpoint and service

--- test/tax-lot/tax-lot.e2e-spec.ts
+++ test/tax-lot/tax-lot.e2e-spec.ts
@@ -3,7 +3,10 @@ import { INestApplication } from "@nestjs/common";
 import { Test } from "@nestjs/testing";
 import { TaxLotModule } from "src/tax-lot/tax-lot.module";
 import { TaxLotRepository } from "src/tax-lot/tax-lot.repository";
-import { getTaxLotGeoJsonByBblQueryResponseSchema } from "src/gen";
+import {
+  getTaxLotByBblQueryResponseSchema,
+  getTaxLotGeoJsonByBblQueryResponseSchema,
+} from "src/gen";
 import { TaxLotRepositoryMock } from "./tax-lot.repository.mock";
 import {
   DataRetrievalException,
@@ -25,6 +28,62 @@ describe("TaxLots", () => {
       .compile();
     app = moduleRef.createNestApplication();
     await app.init();
+  });
+
+  describe("getTaxLotByBbl", () => {
+    it("should 200 and return documented schema when finding by valid bbl", async () => {
+      const mock = taxLotRepository.findByBblMocks[0];
+
+      const response = await request(app.getHttpServer())
+        .get(`/tax-lots/${mock.bbl}`)
+        .expect(200);
+      expect(() =>
+        getTaxLotByBblQueryResponseSchema.parse(response.body),
+      ).not.toThrow();
+    });
+
+    it("should 400 when finding by too short bbl", async () => {
+      const shortBbl = "012345678";
+      const response = await request(app.getHttpServer())
+        .get(`/tax-lots/${shortBbl}`)
+        .expect(400);
+      expect(response.body.message).toBe(
+        new InvalidRequestParameterException().message,
+      );
+      expect(response.body.error).toBe(HttpName.BAD_REQUEST);
+    });
+
+    it("should 400 when finding by lettered bbl", async () => {
+      const letterBbl = "012345678Y";
+      const response = await request(app.getHttpServer())
+        .get(`/tax-lots/${letterBbl}`)
+        .expect(400);
+      expect(response.body.message).toBe(
+        new InvalidRequestParameterException().message,
+      );
+      expect(response.body.error).toBe(HttpName.BAD_REQUEST);
+    });
+
+    it("should 404 when finding by missing bbl", async () => {
+      const missingBbl = "0123456789";
+      const response = await request(app.getHttpServer())
+        .get(`/tax-lots/${missingBbl}`)
+        .expect(404);
+      expect(response.body.message).toBe(HttpName.NOT_FOUND);
+    });
+
+    it("should 500 when the database errors", async () => {
+      const dataRetrievalException = new DataRetrievalException();
+      jest.spyOn(taxLotRepository, "findByBbl").mockImplementationOnce(() => {
+        throw dataRetrievalException;
+      });
+      const mock = taxLotRepository.findByBblMocks[0];
+      const response = await request(app.getHttpServer())
+        .get(`/tax-lots/${mock.bbl}`)
+        .expect(500);
+      expect(response.body.message).toBe(dataRetrievalException.message);
+      expect(response.body.error).toBe(HttpName.INTERNAL_SEVER_ERROR);
+    });
   });
 
   describe("getTaxLotGeoJsonByBbl", () => {
