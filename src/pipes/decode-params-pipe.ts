63bb1de6761ae9d5ec3e21ba8694a1fb08d6b85b
tangoyankee <miller.tim108@gmail.com>
2024-03-13 15:09:40 -0400

    Support exploded form array serialization
    - Add an array guard to the decode params pipe that lets
    arrays deserialized by qs pass through safely.
    - Update decode pipe description
    - Update OpenAPI documentation to note that array bracket
    serialization is supported
    
    closes #193

--- src/pipes/decode-params-pipe.ts
+++ src/pipes/decode-params-pipe.ts
@@ -1,4 +1,5 @@
 import { PipeTransform } from "@nestjs/common";
+import { InvalidRequestParameterException } from "src/exception";
 import { ZodObject, ZodRawShape } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 
@@ -15,9 +16,11 @@ type JsonSchema =
 
 /**
  * Parameters sent in a url, such as path and query parameters, are encoded as strings.
- * Also, arrays follow a "simple, exploded" format. For example [0,1,2] is sent in the
- * url as "0,1,2". The DecodeParamsPipe converts the parameters from their string encoding
- * to their open-api documented type.
+ * Also, arrays primarily follow a "form, non-exploded" format. For example [0,1,2] is sent in the
+ * url as "0,1,2". (Though in some cases, client tools will serialize the array in "form, exploded" format,
+ * with brackets appended to the parameter name. For example, param[]=0,param[]=1.)
+ * The DecodeParamsPipe converts the parameters from their string encoding to their open-api documented type.
+ * It supports both "form, non-exploded" and "form, exploded" serialization formats.
  *
  * The zodToJsonSchema creates an easy-to-parse json object that describes the documented type.
  * When the type should be a number, it is coerced to a number or NaN. When the type should be array,
@@ -30,7 +33,7 @@ type JsonSchema =
 export class DecodeParamsPipe<T extends ZodRawShape> implements PipeTransform {
   constructor(private schema: ZodObject<T>) {}
 
-  transform(params: Record<string, string>) {
+  transform(params: Record<string, string | string[]>) {
     const schemaProperties = (
       zodToJsonSchema(this.schema) as {
         properties: Record<string, JsonSchema | undefined>;
@@ -44,9 +47,14 @@ export class DecodeParamsPipe<T extends ZodRawShape> implements PipeTransform {
       ) => {
         const properties = schemaProperties[param];
         if (properties?.type === "number") {
+          // Guard against the parameter being formatted in such a way that it was deserialized upstream into an array
+          if (Array.isArray(value))
+            throw new InvalidRequestParameterException();
           config[param] = parseFloat(value);
         } else if (properties?.type === "array") {
-          const values = value.split(",");
+          // with formats like arrayBracket, the array was already created upstream;
+          // array creation can be skipped
+          const values = Array.isArray(value) ? value : value.split(",");
           config[param] =
             properties.items.type === "number"
               ? values.map((value) => parseFloat(value))
