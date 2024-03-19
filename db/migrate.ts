11fa2b7b4c96ec74418b344214289dd0b669f9e7
Damon McCullough <damonmcc1391@gmail.com>
2024-03-19 17:10:17 -0400

    ensure non-admin user creates tables in dev databases

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,14 +1,14 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { exit } from "process";
-import { pgClientAdmin } from "./pg-client";
+import { pgClient } from "./pg-client";
 
 (async () => {
   console.info("start migrate");
-  await pgClientAdmin.connect();
-  const db = drizzle(pgClientAdmin);
+  await pgClient.connect();
+  const db = drizzle(pgClient);
   await migrate(db, { migrationsFolder: "./db/migration" });
-  await pgClientAdmin.end();
+  await pgClient.end();
   console.info("end migrate");
   exit();
 })();
