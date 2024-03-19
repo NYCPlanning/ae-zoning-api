244b55b0b3bc9168d01575ebcc022886e5a4f16b
Damon McCullough <damonmcc1391@gmail.com>
2024-03-18 21:21:42 -0400

    use admin db credentials in migrations
    This change is needed to run migrations on the database cluster in DO.

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,14 +1,14 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { exit } from "process";
-import { pgClient } from "./pg-client";
+import { pgClientAdmin } from "./pg-client";
 
 (async () => {
   console.info("start migrate");
-  await pgClient.connect();
-  const db = drizzle(pgClient);
+  await pgClientAdmin.connect();
+  const db = drizzle(pgClientAdmin);
   await migrate(db, { migrationsFolder: "./db/migration" });
-  await pgClient.end();
+  await pgClientAdmin.end();
   console.info("end migrate");
   exit();
 })();
