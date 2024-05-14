49589a5d2be376531cf32cb3f9a235e8a2b53d89
Bryan Marchena <bmarchena@planning.nyc.gov>
2024-05-14 09:12:23 -0400

    Add schemas for city council and community districts

---
+++ src/schema/commmunity-district.ts
@@ -0,0 +1,25 @@
+import { char, pgTable, primaryKey } from "drizzle-orm/pg-core";
+import { borough } from "./borough";
+import { multiPointGeom, multiPolygonGeom, pointGeom } from "src/drizzle-pgis";
+import { z } from "zod";
+
+export const communityDistrict = pgTable(
+  "community_district",
+  {
+    boroughId: char("borough_id", { length: 1 }).references(() => borough.id),
+    id: char("id", { length: 2 }),
+    liFt: multiPointGeom("li_ft", 2263),
+    mercatorFill: multiPolygonGeom("mercator_fill", 3857),
+    mercatorLabel: pointGeom("mercator_label", 3857),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.boroughId, table.id] }),
+    };
+  },
+);
+
+export const communityDistrictEntitySchema = z.object({
+  boroughId: z.string().length(1),
+  id: z.string().length(2),
+});
