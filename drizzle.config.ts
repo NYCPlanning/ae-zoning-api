9f832e1c63a32c8180e2a946a8c47b403d9e5040
tangoyankee <miller.tim108@gmail.com>
2024-06-28 15:32:02 -0400

    Database environment for ssl config
    Update database ssl config to use database specific flag
    
    closes #336

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,7 +11,7 @@ export default {
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME!,
-    ssl: process.env.NODE_ENV === "production" && {
+    ssl: process.env.DATABASE_ENV !== "development" && {
       rejectUnauthorized: false,
     },
   },
