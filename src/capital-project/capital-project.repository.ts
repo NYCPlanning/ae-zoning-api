732541c366fdd5eb1beb082db9e611cee064782c
Timothy Miller <miller.tim108@gmail.com>
2025-02-24 13:53:24 -0500

    Agency budgets in capital project tiles
    Add agency budgets as JSON arrays in tiles for capital projects
    
    closes #427

--- src/capital-project/capital-project.repository.ts
+++ src/capital-project/capital-project.repository.ts
@@ -324,6 +324,11 @@ export class CapitalProjectRepository {
           commitmentsTotal: sum(capitalCommitmentFund.value).as(
             "commitmentsTotal",
           ),
+          agencyBudgets: sql<
+            Array<string>
+          >`TRANSLATE(ARRAY_AGG(DISTINCT ${capitalCommitment.budgetLineCode})::text, '{}', '')`.as(
+            "agencyBudgets",
+          ),
           geom: sql<string>`
             CASE
               WHEN ${capitalProject.mercatorFillMPoly} && ST_TileEnvelope(${z},${x},${y})
