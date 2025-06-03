bb5f964d8a50180960de5b195ef8a1ae86593b86
Timothy Miller <miller.tim108@gmail.com>
2025-06-03 11:33:33 -0400

    coerce booleans within transform pipe

--- src/pipes/zod-transform-pipe.ts
+++ src/pipes/zod-transform-pipe.ts
@@ -1,6 +1,6 @@
 import { PipeTransform } from "@nestjs/common";
 import { InvalidRequestParameterException } from "src/exception";
-import { ZodRawShape, ZodObject, ZodOptional, ZodArray } from "zod";
+import { ZodRawShape, ZodObject, ZodOptional, ZodArray, ZodBoolean } from "zod";
 export class ZodTransformPipe<T extends ZodRawShape> implements PipeTransform {
   constructor(private schema: ZodObject<T> | ZodOptional<ZodObject<T>>) {}
 
@@ -14,18 +14,31 @@ export class ZodTransformPipe<T extends ZodRawShape> implements PipeTransform {
 
     const decodedParams: Record<
       string,
-      string | number | Array<string> | Array<number>
+      boolean | string | number | Array<string> | Array<number>
     > = {};
 
     Object.entries(params).forEach(([param, value]) => {
       const schema = schemaProperties[param];
       const property = schema instanceof ZodOptional ? schema.unwrap() : schema;
 
-      decodedParams[param] = !(property instanceof ZodArray)
-        ? value
-        : Array.isArray(value)
-          ? value
-          : value.split(",");
+      if (property instanceof ZodArray) {
+        decodedParams[param] = Array.isArray(value) ? value : value.split(",");
+        return;
+      }
+
+      if (property instanceof ZodBoolean) {
+        if (value === "true") {
+          decodedParams[param] = true;
+          return;
+        }
+        if (value === "false") {
+          decodedParams[param] = false;
+          return;
+        }
+        throw new InvalidRequestParameterException();
+      }
+
+      decodedParams[param] = value;
     });
 
     try {
