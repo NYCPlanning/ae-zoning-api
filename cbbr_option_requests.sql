235b8a00a113613a93ab0a79a819ec6db8732407
Timothy Miller <miller.tim108@gmail.com>
2025-07-23 14:43:10 -0400

    queries with separate agency needs_group relationship

--- cbbr_option_requests.sql
+++ cbbr_option_requests.sql
@@ -1,20 +1,38 @@
 -- Agency dropdown filtered to those with community board budget requests
 SELECT
-	initials,
-	name
+	agency.initials,
+	agency.name
 FROM agency
-WHERE
-	community_board_budget_request_needs_group_id IS NOT NULL
+LEFT JOIN agency_needs_group ON
+		agency_needs_group.agency_initials = agency.initials
+LEFT JOIN needs_group ON
+		needs_group.id = agency_needs_group.needs_group_id
+WHERE agency_needs_group.needs_group_id IS NOT NULL
+ORDER BY agency.initials;
 
 -- Agency dropdown filtered with needs group selected
-SELECT initials, name FROM agency
-WHERE community_board_budget_request_needs_group_id = 3;
+SELECT
+	agency.initials,
+	agency.name
+FROM agency
+LEFT JOIN agency_needs_group ON
+	agency_needs_group.agency_initials = agency.initials
+LEFT JOIN needs_group ON
+	needs_group.id = agency_needs_group.needs_group_id
+WHERE agency_needs_group.needs_group_id = 10
+ORDER BY agency.initials;
 
 -- Agency dropdown filtered with policy area selected
-SELECT initials, name FROM agency
-LEFT JOIN needs_group
-	ON needs_group.id = community_board_budget_request_needs_group_id
-WHERE policy_area_id = 2;
+SELECT
+	agency.initials,
+	agency.name
+FROM agency
+LEFT JOIN agency_needs_group ON
+	agency_needs_group.agency_initials = agency.initials
+LEFT JOIN needs_group ON
+	needs_group.id = agency_needs_group.needs_group_id
+WHERE needs_group.policy_area_id = 2
+ORDER BY agency.initials;
 
 -- Policy area dropdown
 SELECT
@@ -38,24 +56,22 @@ SELECT
 FROM policy_area
 LEFT JOIN needs_group
 	ON needs_group.policy_area_id = policy_area.id
-LEFT JOIN agency
-	ON agency.community_board_budget_request_needs_group_id = needs_group.id
-WHERE agency.initials = 'DOE';
-
-SELECT needs_group.policy_area_id FROM needs_group  WHERE policy_area_id = 1;
+LEFT JOIN agency_needs_group
+	ON agency_needs_group.needs_group_id = needs_group.id
+WHERE agency_needs_group.agency_initials = 'DOE';
 
 -- Needs group dropdown
 SELECT id, description FROM needs_group;
 
 -- Needs group dropdown filtered for policy area
 SELECT id, description FROM needs_group
-WHERE policy_area_id = 2;
+WHERE needs_group.policy_area_id = 2;
 
 -- Needs group downdown filtered for agencies
 SELECT
 	needs_group.id,
 	needs_group.description
 FROM needs_group
-LEFT JOIN agency ON
-	agency.community_board_budget_request_needs_group_id = needs_group.id
-WHERE agency.initials = 'DOT';
+LEFT JOIN agency_needs_group ON
+	agency_needs_group.needs_group_id = needs_group.id
+WHERE agency_needs_group.agency_initials = 'DOT';
