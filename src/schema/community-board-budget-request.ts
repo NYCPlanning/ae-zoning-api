5c173ee8381e01b5352c315d265c55d205cc922d
pratishta <ypradhi9@gmail.com>
2025-08-27 17:35:35 -0400

    Add cbbr tables to zoning api

--- src/schema/community-board-budget-request.ts
+++ src/schema/community-board-budget-request.ts
@@ -1,6 +1,10 @@
-import { check, pgTable, smallint, text } from "drizzle-orm/pg-core";
+import { char, check, pgTable, smallint, text } from "drizzle-orm/pg-core";
 import { agency } from "./agency";
 import { sql } from "drizzle-orm";
+import { borough } from "./borough";
+import { communityDistrict } from "./community-district";
+import { managingCode } from "./managing-code";
+import { geometryCollectionGeom } from "src/drizzle-pgis";
 
 export const cbbrPolicyArea = pgTable("cbbr_policy_area", {
   id: smallint("id").generatedByDefaultAsIdentity().primaryKey(),
@@ -48,4 +52,47 @@ export const cbbrOptionCascade = pgTable(
       sql`${table.type} IN ('Capital', 'Expense')`,
     ),
   ],
+);
+
+export const cbbrAgencyCategoryResponse = pgTable(
+  "cbbr_agency_category_response",
+  {
+    id: smallint("id").generatedByDefaultAsIdentity().primaryKey(),
+    description: text("description").notNull(),
+  },
+);
+
+export const communityBoardBudgetRequest = pgTable(
+  "community_board_budget_requests",
+  {
+    id: text("id").notNull(),
+    trackingCode: text("tracking_code").notNull(),
+    boroughId: char("borough_id", { length: 1 })
+      .notNull()
+      .references(() => borough.id),
+    communityDistrictId: char("community_district_id", { length: 2 })
+      .notNull()
+      .references(() => communityDistrict.id),
+    agency: text("agency")
+      .notNull()
+      .references(() => agency.initials),
+    managingCode: char("managing_code", { length: 3 })
+      .notNull()
+      .references(() => managingCode.id),
+    agencyCategoryResponse: text("agency_category_response")
+      .notNull()
+      .references(() => cbbrAgencyCategoryResponse.description),
+    agencyResponse: text("agency_response").notNull(),
+    requestType: text("requestType").notNull(),
+    priority: smallint("priority").notNull(),
+    need: smallint("need_id")
+      .notNull()
+      .references(() => cbbrNeed.id),
+    request: smallint("request_id")
+      .notNull()
+      .references(() => cbbrRequest.id),
+    explanation: text("explanation").notNull(),
+    isLocationSpecific: text("is_location_specific").notNull(),
+    geom: geometryCollectionGeom("geom", 4326),
+  },
 );
