cd65671258582b556906295b165e32aa5b93ab5c
Timothy Miller <miller.tim108@gmail.com>
2025-11-19 17:31:24 -0500

    Add location fields to budget requests
    Update community board budget requests with location fields
    
    closes #523

---
+++ db/migration/0028_last_sersi.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "community_board_budget_request" ADD COLUMN "address" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "site_name" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "segment_on_street" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "segment_cross_street_one" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "segment_cross_street_two" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "intersection_street_one" text;--> statement-breakpoint
+ALTER TABLE "community_board_budget_request" ADD COLUMN "intersection_street_two" text;
