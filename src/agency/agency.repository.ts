0519e799edca73a3bc9740c332b1abe1c399cbf4
Timothy Miller <miller.tim108@gmail.com>
2025-11-20 15:12:12 -0500

    upgrade run time dependencies

--- src/agency/agency.repository.ts
+++ src/agency/agency.repository.ts
@@ -28,8 +28,8 @@ export class AgencyRepository {
       domain: "agency",
       function: "checkByInitials",
     });
-    const cachedValue: boolean | null = await this.cacheManager.get(key);
-    if (cachedValue !== null) return cachedValue;
+    const cachedValue = await this.cacheManager.get<boolean>(key);
+    if (cachedValue !== undefined) return cachedValue;
     try {
       const result = await this.#checkByInitials.execute({
         initials,
