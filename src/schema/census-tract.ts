8f52ef3336ff61df56272919ea0f8283ca93bde9
pratishta <ypradhi9@gmail.com>
2026-04-13 10:50:13 -0400

    Add census tract schema
    Redo migrations
    
    Re generate and run migrations
    
    Fix entity schema name

---
+++ src/schema/census-tract.ts
@@ -0,0 +1,31 @@
+import {
+  pgTable,
+  smallint,
+  text,
+  primaryKey,
+  index,
+} from "drizzle-orm/pg-core";
+import { multiPolygonGeom } from "src/drizzle-pgis";
+import z from "zod";
+
+export const censusTract = pgTable(
+  "census_tract",
+  {
+    label: text("label").notNull(),
+    year: smallint("year").notNull(),
+    liFt: multiPolygonGeom("li_ft", 2263),
+    mercatorFill: multiPolygonGeom("mercator_fill", 3857),
+  },
+  (table) => [
+    primaryKey({ columns: [table.label, table.year] }),
+    index().using("GIST", table.liFt),
+    index().using("GIST", table.mercatorFill),
+  ],
+);
+
+export const censusTractEntitySchema = z.object({
+  label: z.string(),
+  year: z.number().int(),
+});
+
+export type CensusTractEntity = z.infer<typeof censusTractEntitySchema>;
