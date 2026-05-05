0d13fcb2be5b0949b3f81c6015cd02d96f2264bf
horatio <horatiorosa@gmail.com>
2026-05-05 10:46:40 -0400

    Added boroughId to census_tract table to be used as part of the composite primary key.

--- src/schema/census-tract.ts
+++ src/schema/census-tract.ts
@@ -4,26 +4,32 @@ import {
   text,
   primaryKey,
   index,
+  char,
 } from "drizzle-orm/pg-core";
+import { borough, boroughEntitySchema } from "./borough";
 import { multiPolygonGeom } from "src/drizzle-pgis";
 import z from "zod";
 
 export const censusTract = pgTable(
   "census_tract",
   {
+    boroughId: char("borough_id", { length: 1 })
+      .notNull()
+      .references(() => borough.id),
     label: text("label").notNull(),
     year: smallint("year").notNull(),
     liFt: multiPolygonGeom("li_ft", 2263),
     mercatorFill: multiPolygonGeom("mercator_fill", 3857),
   },
   (table) => [
-    primaryKey({ columns: [table.label, table.year] }),
+    primaryKey({ columns: [table.boroughId, table.label, table.year] }),
     index().using("GIST", table.liFt),
     index().using("GIST", table.mercatorFill),
   ],
 );
 
 export const censusTractEntitySchema = z.object({
+  boroughId: boroughEntitySchema.shape.id,
   label: z.string(),
   year: z.number().int(),
 });
