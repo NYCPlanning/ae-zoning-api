9a7d8ef1588dd9243092a8ee7b2a8b3e7581b05c
pratishta <ypradhi9@gmail.com>
2026-06-16 12:52:05 -0400

    Add sgr letter and year columns

---
+++ db/migration/0041_sloppy_omega_red.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "facility" ADD COLUMN "sgr_ltr" char(1);--> statement-breakpoint
+ALTER TABLE "facility" ADD COLUMN "sgr_arc_ltr" char(1);--> statement-breakpoint
+ALTER TABLE "facility" ADD COLUMN "sgr_sys_ltr" char(1);--> statement-breakpoint
+ALTER TABLE "facility" ADD COLUMN "sgr_year" smallint;
