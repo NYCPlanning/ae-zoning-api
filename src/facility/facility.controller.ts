b411ef2ae8a939e61d775bc3faf9e65b75d678bc
David Hochbaum <dhochbaum@planning.nyc.gov>
2026-08-07 16:02:34 -0400

    Update headers for CSV export SGR columns

--- src/facility/facility.controller.ts
+++ src/facility/facility.controller.ts
@@ -120,7 +120,7 @@ export class FacilityController {
       buffer,
     });
 
-    const csvData = `ID,Name,Address,BIN,BBL,Oversight Agency,Jurisdiction,Operator Type,Operator Name,Category,Category Group,Category Subgroup,sgrLtr,sgrArcLtr,sgrSysLtr,sgrYear\n${unparse(data, { header: false })}`;
+    const csvData = `ID,Name,Address,BIN,BBL,Oversight Agency,Jurisdiction,Operator Type,Operator Name,Category,Category Group,Category Subgroup,SGR Letter Grade - Total,SGR Letter Grade - Architectural,SGR Letter Grade - Systems / M&E,SGR Assessment Year\n${unparse(data, { header: false })}`;
 
     res.set("Content-Type", "application/csv");
     res.set(
